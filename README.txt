cb3e291d44585ee614890532fded697b8bae4d1c
Javer <secu1jv@gmail.com>
2021-10-05 17:33:17 -0400

    v 1.0Alpha

---
+++ README.txt
@@ -0,0 +1,11 @@
+##How to start the athena app
+
+#npm run but before
+
+but before you need to install all dependencies about npm scripts.
+
+## install all dependecies with this command
+
+npm install 
+
+#run command npm start in both files.
---
+++ backend/index.js
@@ -0,0 +1,29 @@
+
+import express from 'express';
+//import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+dotenv.config();
+
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Atenea memories API');
+})
+
+//const CONNECTION_URL = 'mongodb+srv://fullstack:12123434@mern.ap9op.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
